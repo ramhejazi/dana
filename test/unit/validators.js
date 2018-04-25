df88d9426faa9212debbdc9274c64c89a9d5a7c7
ramhejazi <htmlpack@gmail.com>
2018-04-25 16:17:13 +0000

    laundry

--- test/unit/validators.js
+++ test/unit/validators.js
@@ -1,8 +1,8 @@
 /**
-	Self-validation of validators!
-	This script creates 2 mocha tests for each dana validator.
-	The validators have 2 fields `i`nvalids and `v`alides parameters 
-	that are used for making tests.
+ *	Self-validation of validators!
+ *	This script creates 2 mocha tests for each dana validator.
+ *	The validators have 2 fields `invalids` and `valides` parameters 
+ *	that are used for making tests dynamically.
 */
 
 const helpers = require('../../src/lib/helpers');
@@ -11,7 +11,7 @@ const validatorsDirPath = require('path').join(__dirname, '../../src/validators'
 helpers.readDirSync(validatorsDirPath).forEach((file) => {
 	const { src, name } = file;
 	const validator = src.handler;
-	const { invalids, valids} = src;
+	const { invalids, valids } = src;
 	if ( !invalids && !valids ) return;
 	describe(`Validator ${name}`, function() {
 		if ( valids ) {
