1a881e5cd25f27402363410495316f88eabebc38
ramhejazi <htmlpack@gmail.com>
2018-04-26 21:53:53 +0000

    adds some comments and some refactoring

--- bin/cli.js
+++ bin/cli.js
@@ -1,48 +1,56 @@
 #!/usr/bin/env node
 
-const Liftoff = require('liftoff');
-const commander = require('commander');
-const path = require('path');
-const chalk = require('chalk');
-const bluebird = require('bluebird');
-const tildify = require('tildify');
-const argv = require('minimist')(process.argv.slice(2));
-// const cliff = require('cliff');
-const pkg = require('../package');
-const helpers = require('../src/lib/helpers');
-const log = require('../src/lib/log');
-const _ = require('lodash');
-const { DanaError } = require('../src/errors');
-const fs = require('fs-extra');
-const ValidationError = require('mitra').ValidationError;
+const Liftoff 				= require('liftoff')
+	, ValidationError 	= require('mitra').ValidationError
+	, DanaError 				= require('../src/errors').DanaError
+	, _ 								= require('lodash')
+	, fs 								= require('fs-extra')
+	, commander					= require('commander')
+	, path							= require('path')
+	, chalk							= require('chalk')
+	, bluebird					= require('bluebird')
+	, tildify						= require('tildify')
+	, argv							= require('minimist')(process.argv.slice(2))
+	, pkg								= require('../package')
+	, helpers						= require('../src/lib/helpers')
+	, log 							= require('../src/lib/log');
 
+/**
+ * An utility object
+ */
 const util = {
+	/**
+	 	* env's value is set by using liftoff's environment
+	 	*/
 	env: null,
 	Promise: bluebird,
-	argv,
-	// cliff,
-	tildify,
-	path,
-	chalk,
-	log,
-	fs,
+	argv, tildify, path, chalk, log, fs,
 
-	echoMitraValidationError(error) {
+	/**
+ 	* Log mitra's validation errors
+ 	* @param {object} mitraError instance of mitra ValidationError class
+ 	*/
+	echoMitraValidationError(mitraError) {
 		log.fail('Validation Error:');
-		if ( error.file ) {
-			log.fail(`file: ${error.file.path}`);
+		if ( mitraError.file ) {
+			log.fail(`file: ${mitraError.file.path}`);
 		}
-		_.each(error.errors, (message, key) => {
+		_.each(mitraError.errors, (message, key) => {
 			log.fail(`${chalk.bold(key)}: ${message}`);
 		});
 	},
 
+	/**
+	 * Exit from current nodejs process
+	 * @param {object|string} e an error object or an error message
+	 */
 	exit(e) {
 		if ( e instanceof DanaError ) {
 			log.fail(`${e.name}: ${e.message || e}`);
-			if (e.file)
-				log.fail(`file: ${e.file.path}`);
-		} else if (e instanceof ValidationError ) {
+			if ( e.file ) {
+				log.fail(`\nfile: ${e.file.path}`);
+			}
+		} else if ( e instanceof ValidationError ) {
 			this.echoMitraValidationError(e);
 		} else {
 			console.log(e);
@@ -50,13 +58,24 @@ const util = {
 		process.exit(1);
 	},
 
+	/**
+	 * Ensure dana module has been installed locally in project directory
+	 * CURRENTLY UNUSED and should be removed!
+	 */
 	ensureLocalModule() {
-		if (!this.env.modulePath) {
+		if ( !this.env.modulePath ) {
 			this.log.fail('No local dana install found in: ' + chalk.magenta(tildify(this.env.cwd)));
 			this.exit('Try installing dana by using the following command: "npm install dana".');
 		}
 	},
 
+	/**
+	 * Log messsages
+	 *
+	 * @param {array} messages
+	 * @param {string} messages[].type type of the message
+	 * @param {string} messages[].message message content 
+	 */
 	logMessages(messages = []) {
 		if ( Array.isArray(messages) ) {
 			return messages.forEach(m => {
@@ -68,29 +87,31 @@ const util = {
 		}
 	},
 
+	/**
+ 	* Create a dana instance for the set environment
+	* @todo validate danafile contents!
+ 	* @return {object} a dana instance
+ 	*/
 	getDanaIns() {
 		const env = this.env;
+
 		// REMOVING LOCAL DEPENDENCY! Reason: Why does it have to be unnecessarily complicated?
 		// this.ensureLocalModule();
-		if (!env.configPath) {
+
+		if ( !env.configPath ) {
 			this.exit('No danafile found in this directory. Specify a path with --danafile');
 		}
-		if (process.cwd() !== env.cwd) {
+
+		if ( process.cwd() !== env.cwd ) {
 			process.chdir(env.cwd);
 			log.info('Working directory changed to ' + chalk.magenta(tildify(env.cwd)));
 		}
-		var environment = commander.env || process.env.NODE_ENV;
-		var defaultEnv = 'development';
-		var config = require(env.configPath);
 
-		if (!environment && typeof config[defaultEnv] === 'object') {
-			environment = defaultEnv;
-		}
+		const
+			environment  = ( commander.env || process.env.NODE_ENV || 'development' )
+			, danaFile = require(env.configPath);
 
-		if (environment) {
-			// log.info('Using environment: ' + environment);
-			config = config[environment];
-		}
+		const config = danaFile[environment];
 
 		if (!config) {
 			log.fail('Unable to read danafile config!');
@@ -100,7 +121,7 @@ const util = {
 		if (argv.debug !== undefined) config.debug = argv.debug;
 		// use the current globally installed dana!
 		var dana = require('../src/dana');
-		config.modulePath = env.modulePath;
+		// config.modulePath = env.modulePath;
 		config.baseDir = env.cwd;
 		return dana(config, environment);
 	}
@@ -112,12 +133,17 @@ function actionWrapper(handler) {
 	};
 }
 
+/**
+ * Callback of the `liftoff.launch` function.
+ * @param {object} env
+ * @param {string} env.cwd absolute path of working directory
+ * @param {string} env.configPath absolute path of danafile.js
+ * @param {string} env.modulePackage dana modules's package.json
+ */
 function invoke(env) {
 	util.env = env;
 	commander
-		.version(
-			pkg.version
-		)
+		.version( pkg.version )
 		.option('--danafile [path]', 'Specify the danafile path.')
 		.option('--cwd [path]', 'Specify the working directory.')
 		.option('--env [name]', 'environment, default: process.env.NODE_ENV || development');
