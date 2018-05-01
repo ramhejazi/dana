72784f75ddfea3abd43b9ca21ecd764a81db8971
ramhejazi <htmlpack@gmail.com>
2018-05-02 04:10:02 +0430

    remove redundant comments

--- src/validators/sql_indexes.js
+++ src/validators/sql_indexes.js
@@ -1,17 +1,3 @@
-/**
- * Adds two numbers.
- *
- * @since 3.4.0
- * @category Math
- * @param {number} augend The first number in an addition.
- * @param {number} addend The second number in an addition.
- * @returns {number} Returns the total.
- * @example
- *
- * add(6, 4)
- * // => 10
- */
-
 const _ = require('lodash');
 const { getType } = require('../lib/helpers');
 
